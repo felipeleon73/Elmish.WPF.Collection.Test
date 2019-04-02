a437b278d1fbd9cb4fbb39b239b194d4d3fc1364
Filippo Bozzi <fbozzi@ambientesc.it>
2019-04-02 18:42:40 +0200

    Aggiungere i file di progetto.

---
+++ ElimishWPFCollection/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace ElimishWPFCollection.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("ElimishWPFCollection")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("ElimishWPFCollection")>]
+[<assembly: AssemblyCopyright("Copyright ©  2019")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("aca04782-274f-417c-96cd-7a226c908f3c")>]
+
+// Version information for an assembly consists of the following four values:
+//
+//       Major Version
+//       Minor Version
+//       Build Number
+//       Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ ElimishWPFCollection/Program.fs
@@ -0,0 +1,68 @@
+﻿module SingleCounter.Program
+
+open System
+
+type Item = 
+  { Id: Guid
+    Name: string }
+
+type Model =
+  { Count: int
+    Items: Item list 
+    Selected: Item option}
+
+let data = 
+    [1..2]
+    |> List.map (fun i -> { Id=Guid.NewGuid(); Name=sprintf "Item %i" i }) 
+
+let init () =
+  { Count = data.Length
+    Items = data 
+    Selected = None}
+
+type Msg =
+  | Update of Guid*string
+  | Select of Item option
+
+let setNewVal id (value: string) items=
+    let updateIf item =
+        if item.Id = id then { item with Name = value.ToUpper() } else item
+    items |> List.map updateIf
+
+let update msg m =
+  match msg with
+  | Update (id,value) -> { m with Items = m.Items |> setNewVal id value }
+  | Select value -> { m with Selected=value }
+
+open Elmish.WPF
+open Elmish.WPF.Internal
+
+let itemBindings () : Internal.BindingSpec<Model*Item,Msg> list = 
+  [
+    "Value" |> Binding.twoWay 
+        (fun m -> (snd m).Name)
+        (fun v m -> Update ((snd m).Id,v) )
+  ]
+
+let bindings model dispatch =
+  [
+    "Count" |> Binding.oneWay (fun m -> m.Count)
+    "Selected" |>  Binding.twoWay 
+                    (fun m -> unbox m.Selected) //<-----------here is the issue! m.Selected is a Model, expect a ViewModel
+                    (fun (v:ViewModel<_,_>) m -> Update v.CurrentModel)
+    "Items" |> Binding.subBindingSeq
+        id
+        (fun m -> m.Items)
+        (fun c -> c.Id)
+        itemBindings
+  ]
+
+open System
+open Elmish
+open Elmish
+
+[<EntryPoint; STAThread>]
+let main argv =
+  Program.mkSimple init update bindings
+  |> Program.withConsoleTrace 
+  |> Program.runWindow (MainWindow())
